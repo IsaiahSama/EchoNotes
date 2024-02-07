aa5a4d8665368c1ae55775f2a7ad4ca7139ce829
IsaiahSama <zeldaplayergl@gmail.com>
2024-02-07 17:18:15 -0400

    Added base endpoints for API

--- Backend/app.py
+++ Backend/app.py
@@ -5,3 +5,19 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/v1/login/")
+async def login():
+    pass 
+
+@app.get("/v1/register/")
+async def register():
+    pass 
+
+@app.post("/v1/transcribe/")
+async def transcribe():
+    pass
+
+@app.get("/v1/converse/")
+async def converse():
+    pass
